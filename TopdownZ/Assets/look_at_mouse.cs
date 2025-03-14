cda05bbe331348b5c868ad96014a7dbe3eb41a55
MiquellaV <arwinvt@gmail.com>
2025-03-14 14:49:47 +0100

    0.4
    MOUSE

---
+++ TopdownZ/Assets/look_at_mouse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class look_at_mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform m_transform;
+    
+    void Start()
+    {
+        m_transform = transform;
+
+        
+    }
+    
+    private void LAmouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint
+            (Input.mousePosition) - m_transform.position;
+            float angle = Mathf.Atan2 (direction.x, direction.y) * Mathf.Rad2Deg;//this is the angle player should rotator towards
+            Quaternion rotation = Quaternion.AngleAxis(angle +90, Vector3.forward); //vector 3 is Z axis
+            m_transform.rotation = rotation;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        LAmouse();
+    }
+}
