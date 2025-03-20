04e945883a5f2ed5add5e03b949263353674601b
coffe3ci <kahveciemirefe@gmail.com>
2025-03-20 14:05:57 +0100

    gameOver menu

---
+++ TopdownZ/Assets/Scripts/tryAgain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tryAgain : MonoBehaviour
+{
+
+    public void TryAgainButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+}
