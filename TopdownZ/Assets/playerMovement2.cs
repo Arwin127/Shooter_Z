0dfa3d1992204bb2a337d8f1586a2b0ae8ec4285
coffe3ci <kahveciemirefe@gmail.com>
2025-03-21 12:27:35 +0100

    2.5
    Co-Authored-By: MiquellaV <94910336+MiquellaV@users.noreply.github.com>

---
+++ TopdownZ/Assets/playerMovement2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A ve D için -1 ile 1 arası değer döner
+        float moveZ = Input.GetAxis("Vertical");   // W ve S için -1 ile 1 arası değer döner
+
+        Vector3 move = new Vector3(moveX, 0, moveZ) * speed;
+        rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+    }
+}
